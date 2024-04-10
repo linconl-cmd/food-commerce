2dd609183c8f4c8cb756ebe06e38b42a7b134304
linconl <linconl@pop-os.localdomain>
2024-04-09 22:32:40 -0300

    Update project

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,7 +1,8 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
+import { IMaskInput } from "react-imask";
 
 import { Head } from "../../components/Head";
 import { PayOrder } from "../../components/OrderCloseAction/PayOrder";
@@ -21,6 +22,7 @@ type FieldValues = yup.InferType<typeof schema>
 
 export default function Payment() {
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },} = useForm<FieldValues>({
@@ -38,21 +40,45 @@ export default function Payment() {
 
         <div className="field">
           <label htmlFor="fullName">Nome e sobrenome </label>
-          <input type="text" id="fullName" autoComplete="name" {...register("fullName")} />
+          <Controller
+            name="fullName"
+            control={control}
+            render={({field}) =>(
+              <input type="text" id="fullName" autoComplete="name" {...field} />
+            )}
+            />
           {errors.fullName && <p className="error">{errors.fullName.message}</p>}
         </div>
 
        <div className="grouped">
         <div className="field">
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email"  autoComplete="email" {...register("email")} />
+            <Controller
+            name="email"
+            control={control}
+            render={({field}) =>(
+              <input type="email" id="email" autoComplete="email" {...field} />
+            )}
+            />
+
             {errors.email && <p className="error">{errors.email.message}</p>}
           </div>
 
 
           <div className="field">
             <label htmlFor="mobile">Celular</label>
-            <input type="tel" id="mobile"  autoComplete="phone"  {...register("mobile")}/>
+            <Controller
+            name="mobile"
+            control={control}
+            render={({field}) =>(
+              <IMaskInput
+                type="tel"
+                id="mobile"
+                autoComplete="phone"
+                mask={"(00) 90000-0000"}
+                {...field} />
+            )}
+            />
             {errors.mobile && <p className="error">{errors.mobile.message}</p>}
           </div>
 
